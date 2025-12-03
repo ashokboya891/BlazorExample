f1c1c576a08984bb229500008594884848962580
AshokKumar <ashokboya891@gmail.com>
2025-12-03 19:31:37 +0530

    added new columns

--- EmployeeBlazorApi/Models/Employee.cs
+++ EmployeeBlazorApi/Models/Employee.cs
@@ -5,5 +5,7 @@
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Email { set; get; }
+        public string Address {  set; get; }    
     }
 }
